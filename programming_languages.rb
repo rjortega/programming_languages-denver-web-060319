d191fecdb6d1e40bc2aa507d6479582afce17f6a
Rj <rikrdo.ortega@gmail.com>
2019-05-26 12:11:43 -0600

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,18 +1,23 @@
-# def reformat_languages(languages)
-#   new_hash = {}
-#   languages.each do |types, languages|
-#     new_hash[:languages] = {languages: types}
-#   end
-# end
-
 def reformat_languages(languages)
-  new_languages = {}
-  languages.each do |type, hash|
-    hash.each do |name, attributes|
-      new_languages[name] ||= attributes
-      new_languages[name][:style] ||= []
-      new_languages[name][:style] << type
+  new_hash = {}
+  languages.each do |types, languages|
+    languages.each do |language, attributes|
+      new_hash[language] ||= attributes
+      new_hash[language][:style] ||= []
+      new_hash[language][:style] << types
     end
   end
-  new_languages
+  new_hash
 end
+
+# def reformat_languages(languages)
+#   new_languages = {}
+#   languages.each do |type, hash|
+#     hash.each do |name, attributes|
+#       new_languages[name] ||= attributes
+#       new_languages[name][:style] ||= []
+#       new_languages[name][:style] << type
+#     end
+#   end
+#   new_languages
+# end
